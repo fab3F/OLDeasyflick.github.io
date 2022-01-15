5c759090e6bf29f74832e07fd435a4883e8fed1d
fab3F <96261710+fab3F@users.noreply.github.com>
2022-01-15 15:20:56 +0100

    Batch update

--- projects/js/script.js
+++ projects/js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1920},"pages":[{"id":"page-template","displayName":"template","link":{"linkType":"LinkTypePage","href":"#!page-template"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1920},"pages":[{"id":"page-projekte","displayName":"Projekte","link":{"linkType":"LinkTypePage","href":"#!page-projekte"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
