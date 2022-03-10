afa706ddb31008e2c07d47418d8d87151b86811a
fab3F <96261710+fab3F@users.noreply.github.com>
2022-03-10 18:00:15 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@
 
     window.silex = window.silex || {}
     window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-kontakt","displayName":"Kontakt","link":{"linkType":"LinkTypePage","href":"#!page-kontakt"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-impressum-datenschutz","displayName":"Impressum & Datenschutz","link":{"linkType":"LinkTypePage","href":"#!page-impressum-datenschutz"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+function openDiscord(){
+    window.open("https://discord.gg/dkpKAdvarP", "_blank");
+}
+function download(var link){
+    window.open(link, "_self");
+}
